8d89bd6fee4934cff1d0329c4501df6c6191734e
Luis Fernando Aparecido Pedroso de Barros <lfpedroso@gmail.com>
2021-11-17 20:26:59 -0300

    Jogo finalizado

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function criarCobrinha(){
 }
 
 function drawFood1() {
-    context.fillStyle = "red";
+    context.fillStyle = "green";
     context.fillRect(food1.x, food1.y, box, box)
 }
 
@@ -43,6 +43,14 @@ function iniciarJogo() {
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood1();
